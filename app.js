df0f8ab54e854c5170bdc7d29bbedf999596e8a3
Manish Tinsola <manishtinsola141@gmail.com>
2026-09-17 21:44:24 +0530

    fotter added main

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const frontend="using html css js and react frontend done!!";
 console.log(frontend);
 const payment="Payment getway using cards";
 console.log(payment);
+
+const fotter="Add fotter";
+console.log(fotter);
